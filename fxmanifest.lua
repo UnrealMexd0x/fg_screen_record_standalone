f1b2ed0bca5b99d83d7b468cc1394c3bd6733271
UnrealMexd0x <118627448+UnrealMexd0x@users.noreply.github.com>
2023-11-12 23:59:57 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name "fg_screen_record"
 description "Screen and Record Command for FiveGuard with ACE Permissions"
 author "UnrealMexd0x"
-version "0.0.7"
+version "1.0.3"
 
 server_scripts {
 	'main.lua',
