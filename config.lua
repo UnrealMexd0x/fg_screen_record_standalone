1a6811a5528910e0d1f99932ffee25a2da01c359
UnrealMexd0x <mariusholzer@mail.de>
2023-12-07 08:58:36 +0100

    Delete config.lua

--- config.lua
+++
@@ -1,9 +0,0 @@
-Config = {}
-
--- Your Command
-Config.RecordCommand = "record"
-Config.ScreenCommand = "screenshot"
-
--- Your ACE 'add_ace group.admin test allow'
-Config.RecordCommandAce = "test"
-Config.ScreenCommandAce = "test"
